967f2d2b251c4fb183a3fe3474a45645ea278d06
CamCarr <carrioncamila790@gmail.com>
2025-04-14 22:42:00 -0300

    Trabajo Practico 1

---
+++ 01 Estructuras Secuenciales/carrion_camila_tp1.py
@@ -0,0 +1,70 @@
+# EJERCICIO_1
+# print ("Hola Mundo!")
+
+# EJERCICIO_2
+# nombre = input ("Por favor ingresa tu nombre: ")
+# print (f"Hola, {nombre}!")
+
+# EJERCICIO_3
+# nombre = input ("Ingrese su nombre: ")
+# apellido = input ("Ingrese su apellido: ")
+# edad = input ("Ingrese su edad: ")
+# residencia = input ("Ingrese su lugar de residencia: ")
+# print (f"Hola! Mi nombre es {nombre} {apellido}, tengo {edad} anios, y vivo en {residencia}")
+
+# EJERCICIO_4
+# radio = input("Por favor ingrese el radio del circulo: ")
+# radio = int(radio)
+# area = 3.14 * radio * radio
+# perimetro = 2 * 3.14 * radio
+# print (f"El area del circulo es: {area}")
+# print (f"El perimetro del circulo es: {perimetro}")
+
+# EJERCICIO_5
+# segundos = input ("Por favor ingrese los segundos: ")
+# horas = int(segundos) / 3600
+# print (f"Los {segundos} segundos, corresponden a {horas} horas")
+
+# EJERCICIO_6
+# numero = input ("Por favor ingrese el numero del cual quiere calcular su tabla de multiplicar: ")
+# i = 0
+# numero = int (numero)
+# while i < 10:
+#     i += 1
+#     multiplicacion = i * numero
+#     print (f"{i} * {numero} = {multiplicacion}")
+
+# EJERCICIO_7
+# numero_1 = int (input ("Por favor ingrese el primer numero: "))
+# numero_2 = int (input ("Por favor ingrese el segundo numero: "))
+# suma = numero_1 + numero_2
+# resta = numero_1 - numero_2
+# multiplicacion = numero_2 * numero_1
+# division = numero_1 / numero_2
+# print (f"La suma es = {suma}")
+# print (f"La resta es = {resta}")
+# print (f"La division es = {division}")
+# print (f"La multiplicacion es = {multiplicacion}")
+
+# EJERCICIO_8
+# altura = float(input("Por favor ingrese su altura en metros: "))
+# peso = int(input("Por favor ingrese su peso en kilogramos: "))
+# imc = peso / (altura * altura)
+# print (f"Su indice de masa corporal es: {imc}")
+
+# EJERCICIO_9
+# celcius = int(input("Por favor ingrese los grados celcius: "))
+# farent = 9/5 * celcius + 32
+# print (f"Los grados corresponden a {farent} grados Fahrenteit")
+
+# EJERCICIO_10
+# num1 = int(input("Por favor ingrese un numero: "))
+# num2 = int(input("Por favor ingrese un numero: "))
+# num3 = int(input("Por favor ingrese un numero: "))
+# promedio = (num1 + num2 + num3 ) /3
+# print (f"El promedio es: {promedio}")
+
+    
+
+
+
