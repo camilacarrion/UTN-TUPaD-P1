aaba505694013ec33af727b672e60cf7a3e6940e
CamCarr <carrioncamila790@gmail.com>
2025-05-07 23:18:41 -0300

    Trabajo Practico 5.1

---
+++ 05 Funciones/5.1 LISTAS/carrion_camila_tp5.1.py
@@ -0,0 +1,60 @@
+# TRABAJO PRACTICO 5.1: LISTAS
+
+# EJERCICIO 1:
+# for i in range(4,101,4):
+#     print (i)
+
+# EJERCICIO 2:
+# colores = ['lila','celeste','verde','amarillo','rosa']
+# print (colores[3])
+# print (colores[-2])
+
+# EJERCICIO 3:
+# lista = []
+# lista.append('verde')
+# lista.append('rojo')
+# lista.append('azul')
+# print (lista)
+
+# EJERCICIO 4:
+# animales = ['perro','gato','conejo','pez']
+# animales[1] = 'loro'
+# animales[3] = 'oso'
+# print (animales)
+
+# EJERCICIO 5:
+# El programa en la primer linea muestra una lista de numeros. 
+# En la segunda linea recorre la lista de numeros, selecciona el mayor entre ellos y lo elimina de la lista.
+# En la ultima linea imprime el resultado de la lista.
+
+# EJERCICIO 6:
+# for i in range(10,31,5):
+#     print (i)
+
+# EJERCICIO 7:
+# autos = ['sedan','polo','suran','gol']
+# autos[1:3] = ['sedan','gol']
+# print (autos)
+
+# EJERCICIO 8:
+# dobles = []
+# dobles.append(2*5)
+# dobles.append(2*10)
+# dobles.append(2*15)
+# print (dobles)
+
+# EJERCICIO 9:
+# compras = [['pan','leche'],['arroz','fideos','salsa'],['agua']]
+# compras[2].append('jugo')
+# compras[1][1]='tallarines'
+# compras[0].remove('pan')
+# print (compras)
+
+# EJERCICIO 10:
+# lista_anidada = [15,True,[25.5,57.9,30.6],False]
+# print (lista_anidada)
+
+    
+
+
+
